d354b4371a240fac66dd0848c1b304a1e4d17ccd
한지욱 <wldnrrha98@naver.com>
2023-06-02 09:44:22 +0000

    5월 31일 13주차 홈페이지 수정 완료

--- js/login.js
+++ js/login.js
@@ -1,14 +1,3 @@
-function init(){ // 로그인 폼에 쿠키에서 가져온 아이디 입력
-    let id = document.querySelector("#floatingInput");
-    let check = document.querySelector("#idSaveCheck");
-    let get_id = getCookie("id");
-    
-    if(get_id) { 
-    id.value = get_id; 
-    check.checked = true; 
-    }
-}
-
 function login(){
     let form = document.querySelector("#form_main");
     let id = document.querySelector("#floatingInput");
@@ -27,69 +16,60 @@ function login(){
             setCookie("id", id.value, 0); //날짜를 0 - 쿠키 삭제
     }
     
-    function closePopup() {
-        if (document.getElementById('check_popup').value) {
-            setCookie("id", "N", 1);
-            console.log("쿠키를 설정합니다.");
-            self.close();
-        }
-    }
+function closePopup() {
     
-function deleteCookie(cookieName){
-    var expireDate = new Date();
-    expireDate.setDate(expireDate.getDate() - 1);
-    document.cookie = cookieName + "= " + "; expires=" + expireDate.toGMTString();
+    if (document.getElementById('check_popup').value) {
+        
+        setCookie("id", "N", 1);
+        console.log("쿠키를 설정합니다.");
+        self.close();
+    }
 }
-
-
     
     if(id.value.length === 0 || password.value.length === 0){
         alert("아이디와 비밀번호를 모두 입력해주세요.")
     }else{
+        session_set(); //세션생성
         form.submit();
     }
 }
 
 function logout(){
+    session_del(); //세션 삭제
     location.href='../index.html';
 }
 
 function get_id(){
-    var getParameters = function(paramName){ // 변수 = 함수(이름)
-    var returnValue; // 리턴값을 위한 변수 선언
-    var url = location.href; // 현재 접속 중인 주소 정보 저장
-    var parameters = (url.slice(url.indexOf('?') + 1, url.length)).split('&'); // ?기준 slice 한 후 split 으로 나눔
-        for(var i = 0; i < parameters.length; i++) { 
-		    var varName = parameters[i].split('=')[0];
-            
-            if (varName.toUpperCase() == paramName.toUpperCase()) {
-                returnValue = parameters[i].split('=')[1];
-                return decodeURIComponent(returnValue);
-            // 나누어진 값의 비교를 통해 paramName 으로 요청된 데이터의 값만 return
-		    }
-	    } // 2중 for문 끝
-}; // 함수 끝
-alert(getParameters('id') + '님 방갑습니다!'); // 메시지 창 출력
-}
-
-function setCookie(name, value, expiredays) {
-        var date = new Date();
-        date.setDate(date.getDate() + expiredays);
-        document.cookie = escape(name) + "=" + escape(value) + "; expires=" + date.toUTCString() + "SameSite=None; Secure";        
+    if (true) {
+        decrypt_text();
     }
-
-function getCookie(name) {
-        var cookie = document.cookie;
-        console.log("쿠키를 요청합니다.");
-        if (cookie != "") {
-            var cookie_array = cookie.split("; ");
-            for ( var index in cookie_array) {
-                var cookie_name = cookie_array[index].split("=");
+    else{
+            var getParameters = function(paramName){ // 변수 = 함수(이름)
+            
+            var returnValue; // 리턴값을 위한 변수 선언
+            var url = location.href; // 현재 접속 중인 주소 정보 저장
+            var parameters = (url.slice(url.indexOf('?') + 1, url.length)).split('&'); // ?기준 slice 한 후 split 으로 나눔
                 
-                if (cookie_name[0] == "id") {
-                    return cookie_name[1];
-                }
-            }
-        }
-        return ;
+                for(var i = 0; i < parameters.length; i++) { 
+		            var varName = parameters[i].split('=')[0];
+                    if (varName.toUpperCase() == paramName.toUpperCase()) {
+                        returnValue = parameters[i].split('=')[1];
+                        return decodeURIComponent(returnValue);
+                    // 나누어진 값의 비교를 통해 paramName 으로 요청된 데이터의 값만 return
+                    }
+                } // 2중 for문 끝
+            }; // 함수 끝
+        alert(getParameters('id') + '님 방갑습니다!'); // 메시지 창 출력
+    }
 }
+
+function addJavascript(jsname) {
+    var th = document.getElementsByTagName('head')[0];
+    var s = document.createElement('script');
+    s.setAttribute('type','text/javascript');
+    s.setAttribute('src',jsname);
+    th.appendChild(s);
+}
+addJavascript('/js/security.js'); //암복호화 함수
+addJavascript('/js/session.js'); //세션 함수
+addJavascript('/js/cookie.js'); //쿠키 함수
